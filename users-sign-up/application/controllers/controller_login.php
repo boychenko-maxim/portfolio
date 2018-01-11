16c02156aa0bfa2d2ba8ce2e7bf605a439bf875f
boychenko-maxim <boichenkomaxim@gmail.com>
2018-01-11 08:59:56 +0300

    Update controller_login.php

--- users-sign-up/application/controllers/controller_login.php
+++ users-sign-up/application/controllers/controller_login.php
@@ -4,30 +4,30 @@ class Controller_Login extends Controller
 {
 	function action_index()
 	{
-        session_start();
+		session_start();
 
-        if (isset($_SESSION['userLoggedIn']) && $_SESSION['userLoggedIn']) {
-            redirect('user');
-        }
+		if (isset($_SESSION['userLoggedIn']) && $_SESSION['userLoggedIn']) {
+		    redirect('user');
+		}
 
 		$this->view->generate('login_view.php');
 	}
 
 	function action_canLogin()
-    {
-        $user = $this->users->getUser($_POST['userLogin'], $_POST['userPassword']);
-        $canLogin = $user !== false;
+	{
+		$user = $this->users->getUser($_POST['userLogin'], $_POST['userPassword']);
+		$canLogin = $user !== false;
 
-        if ($canLogin) {
-            session_start();
+		if ($canLogin) {
+			session_start();
 
-            $_SESSION['userLoggedIn'] = true;
-            $_SESSION['userLogin'] = $_POST['userLogin'];
-            $_SESSION['userPassword'] = $_POST['userPassword'];
-            $_SESSION['userCity'] = $user->city;
-            $_SESSION['userAge'] = $user->age;
-        }
+			$_SESSION['userLoggedIn'] = true;
+			$_SESSION['userLogin'] = $_POST['userLogin'];
+			$_SESSION['userPassword'] = $_POST['userPassword'];
+			$_SESSION['userCity'] = $user->city;
+			$_SESSION['userAge'] = $user->age;
+		}
 
-        echo boolToString($canLogin);
-    }
+		echo boolToString($canLogin);
+	}
 }
